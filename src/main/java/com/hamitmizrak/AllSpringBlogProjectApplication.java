77683b43c9686ffdfc0c8b24c7589916c0c19048
hamitmizrak <hamitmizrak@gmail.com>
2022-02-20 18:38:09 +0300

    Security => exclude(dahil etmemek)

--- src/main/java/com/hamitmizrak/AllSpringBlogProjectApplication.java
+++ src/main/java/com/hamitmizrak/AllSpringBlogProjectApplication.java
@@ -1,13 +1,21 @@
 package com.hamitmizrak;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class AllSpringBlogProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AllSpringBlogProjectApplication.class, args);
     }
+
+
+
 
 }
