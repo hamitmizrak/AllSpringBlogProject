286036e1d1ef81df8bad59ad9086bfc16311416a
hamitmizrak <hamitmizrak@gmail.com>
2022-02-20 19:10:21 +0300

    BaseEntityAudit AuditorAwareImpl AuditorAwareBean @SpringBootApplication

--- src/main/java/com/hamitmizrak/AllSpringBlogProjectApplication.java
+++ src/main/java/com/hamitmizrak/AllSpringBlogProjectApplication.java
@@ -1,14 +1,20 @@
 package com.hamitmizrak;
 
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
 )
+
+//Audit aktif etmek icin auditorAware ==> Bean adı
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@Log4j2
 public class AllSpringBlogProjectApplication {
 
     public static void main(String[] args) {
