534790cd86161945bdcc81f5ad4bd14a4f3990aa
hamitmizrak <hamitmizrak@gmail.com>
2022-02-20 19:17:32 +0300

    EmployeeRepository

---
+++ src/main/java/com/hamitmizrak/data/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.hamitmizrak.data.repository;
+
+import com.hamitmizrak.data.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
+}
