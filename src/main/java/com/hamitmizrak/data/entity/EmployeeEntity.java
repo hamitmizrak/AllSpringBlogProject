cc796873b785e5af67c803f1a73206f2071cef97
hamitmizrak <hamitmizrak@gmail.com>
2022-02-20 19:15:36 +0300

    EmployeeEntity

---
+++ src/main/java/com/hamitmizrak/data/entity/EmployeeEntity.java
@@ -0,0 +1,32 @@
+package com.hamitmizrak.data.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
+
+@Entity
+@Table(name="employee")
+public class EmployeeEntity extends BaseEntityAudit implements Serializable {
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email_address")
+    private String emailAddress;
+}
